70147cd7c2bac05f4bd2078c17ec27b4c4bd28eb
priteymehta <priteymehta235@gmail.com>
2018-07-23 04:43:01 +0530

    after add user controller of angular

---
+++ AngularDemo/ngController/UserController.js
@@ -0,0 +1,14 @@
+﻿var mainApp = angular.module("mainApp", []);
+
+mainApp.controller('UserController', function ($scope) {
+    $scope.student = {
+        firstName: "Mahesh",
+        lastName: "Parashar",
+
+        fullName: function () {
+            var studentObject;
+            studentObject = $scope.student;
+            return studentObject.firstName + " " + studentObject.lastName;
+        }
+    };
+});
